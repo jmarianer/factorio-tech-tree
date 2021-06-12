c83f9297c565e85be374dad1dca6c781fd2894cf
Joey Marianer <joey@rescale.com>
2021-06-12 16:43:43 -0700

    Crude approximation of davemcw's tech tree but with arbitrary mods

--- get_factorio_data.py
+++ get_factorio_data.py
@@ -2,6 +2,7 @@ import configparser
 import io
 import json
 import lupa
+import math
 import re
 
 from defines import defines
@@ -222,18 +223,73 @@ data = lua_table_to_python(lua.globals().data.raw)
 # print(json.dumps(data, sort_keys=True, indent=4))
 
 
+# TODO: Here be dragons. This should really be a separate file (actually, everything preceding this should).
 # HTML with embedded images for all techs in dependency order
-tech = data['technology']
+print('''
+<title>Tech Tree</title>
+<link rel="stylesheet" href="https://davemcw.com/factorio/tech-tree/tech-tree.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://davemcw.com/factorio/tech-tree/tech-tree.js" charset="UTF-8"></script>
+</head>
+<body>
+<div id="tooltip">
+<div class="tiptitle"></div>
+<div class="tip1 tipheading">Products:</div>
+<div class="tip2"></div>
+<div class="tip3 tipheading">Ingredients:</div>
+<div class="tip4"></div>
+</div>
+''')
+tech_and_prereqs = {
+        k: set(v.get('prerequisites', []))
+        for k, v in data['technology'].items()}
 prereqs_available = set()
+positions = {}
+top = 34
 while True:
     new_available = {
             k
-            for k, v in tech.items()
-            if set(v.get('prerequisites', [])).issubset(prereqs_available)}
+            for k, v in tech_and_prereqs.items()
+            if v.issubset(prereqs_available)}
     if len(new_available - prereqs_available) == 0:
         break
+
+    left = 20
     for tech_name in new_available - prereqs_available:
         image = image_to_data_url(get_tech_icon(tech_name))
-        name =  localize('technology-name', tech_name)
-        print(f'<img style="width:32; height:32" src="{image}" title="{name}">')
+        title = localize('technology-name', tech_name)
+        prereqs = ','.join(tech_and_prereqs[tech_name])
+        positions[tech_name] = (left, top)
+        print(f'<div style="left:{left}px;top:{top}px" id="{tech_name}" class="tech L1" data-prereqs="{prereqs}" data-title="{title}"><img class="pic" src="{image}"></div>')
+        left += 101
+    top += 168
     prereqs_available.update(new_available)
+
+"""
+wss: 727,34
+mil: 828,202
+
+midpoint: 818,153
+
+767,68
+40, 34
+"""
+
+for t in tech_and_prereqs:
+    for p in tech_and_prereqs[t]:
+        # Draw a line from (x1, y1) to (x2, y2)
+        x1, y1 = positions[t]
+        x2, y2 = positions[p]
+
+        x1 += 40
+        x2 += 40
+        y1 += 34
+        y2 += 34
+
+        xmid = (x1 + x2) / 2
+        ymid = (y1 + y2) / 2
+        angle = math.atan2((y2 - y1), (x2 - x1))
+        length = math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
+
+        left = xmid - length / 2
+        print(f'<div id="{t}_{p}" class="path dark" style="left:{left}px;top:{ymid}px;width:{length}px;transform:rotate({angle}rad)"></div>')
