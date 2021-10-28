9d58e9b3391e9abebf4ce08ff526a226eeeb99e7
Joey Marianer <joey@rescale.com>
2021-10-27 19:26:41 -0700

    Completely change how tooltips work, and other minor HTML/CSS improvements

--- tech-tree.js
+++ tech-tree.js
@@ -1,26 +1,22 @@
 $(function() {
-  // Tooltip for technologies and items
-  let toolTipDiv = $("#tooltip");
-  function setToolTip(source) {
+  function getToolTip(source) {
     source = $(source);
     tech = source.closest('.tech');
     item = source.closest('.item');
-
     source = (item.length == 1) ? item : tech;
-    toolTipDiv.text(source.data('title'));
+    return source.children('.tooltip');
   }
 
   $(".item, .tech").on("mouseover", e => {
     e.stopPropagation();
-    setToolTip(e.target);
-    toolTipDiv.show();
+    getToolTip(e.target).show();
   });
 
   $(".item, .tech").on("mousemove", function(e){
-    toolTipDiv.css({ top: e.pageY - 50, left: e.pageX + 15 });
+    getToolTip(e.target).css({ top: e.pageY - 50, left: e.pageX + 15 });
   });
 
   $(".item, .tech").on("mouseout", function(e){
-    toolTipDiv.hide();
+    getToolTip(e.target).hide();
   });
 });
