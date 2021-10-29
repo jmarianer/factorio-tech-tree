3155723838c54d01db21c82c99edd127459e870d
Joey Marianer <joey@rescale.com>
2021-10-29 08:51:19 -0700

    Partial support for clicking on an item: blur everything but the item and its (direct and indirect) prerequisites

--- tech-tree.js
+++ tech-tree.js
@@ -7,16 +7,40 @@ $(function() {
     return source.children('.tooltip');
   }
 
-  $(".item, .tech").on("mouseover", e => {
+  $('.item, .tech').on('mouseover', e => {
     e.stopPropagation();
     getToolTip(e.target).show();
   });
 
-  $(".item, .tech").on("mousemove", function(e){
+  $('.item, .tech').on('mousemove', e => {
     getToolTip(e.target).css({ top: e.pageY - 50, left: e.pageX + 15 });
   });
 
-  $(".item, .tech").on("mouseout", function(e){
+  $('.item, .tech').on('mouseout', e => {
     getToolTip(e.target).hide();
+  });
+
+  function showTreeUp(tech_name) {
+    let tech = $(document.getElementById('tech_' + tech_name));
+    tech.addClass('selected').removeClass('blurred');
+    let prereqs = tech.data('prerequisites').split(',');
+    for (let prereq of prereqs) {
+      showTreeUp(prereq);
+    }
+  }
+
+  $('.tech').on('click', e => {
+    let source = $(e.target).closest('.tech');
+    e.stopPropagation();
+ 
+    $('.tech').removeClass('selected').addClass('blurred');
+
+    let tech_name = source.data('name');
+    showTreeUp(tech_name);
+    // TODO showTreeDown(tech_name);
+  });
+
+  $('body').on('click', e => {
+    $('.tech').removeClass('selected').removeClass('blurred');
   });
 });
