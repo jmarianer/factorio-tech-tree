82ab338524151fa826095d202afc3536acacb539
Joey Marianer <joey@rescale.com>
2021-11-10 20:29:47 -0800

    Add mypy and add type hints to utils.py

--- utils.py
+++ utils.py
@@ -1,38 +1,52 @@
 import base64
 import io
-import lupa
 import re
+from lupa import LuaRuntime
+from typing import Any
+from PIL.Image import Image
 
 
-def lua_table_to_python(obj):
-    if lupa.lua_type(obj) == 'table':
+# TODO: Figure out if this can be done more elegantly
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from lupa import LuaObject
+else:
+    LuaObject = int
+
+
+def lua_table_to_python(obj: LuaObject) -> Any:
+    if isinstance(obj, int) or isinstance(obj, str) or isinstance(obj, float) or isinstance(obj, bool) or obj is None:
+        return obj
+    else:
         if all(isinstance(i, int) for i in obj.keys()):
             return [lua_table_to_python(v) for v in obj.values()]
         else:
             return {str(k): lua_table_to_python(v) for k, v in obj.items()}
-    elif lupa.lua_type(obj) is not None:
-        return lupa.lua_type(obj)
-    else:
-        return obj
 
 
-def python_to_lua_table(lua, obj):
+def python_to_lua_table(lua: LuaRuntime, obj: Any) -> LuaObject:
     if isinstance(obj, dict):
         return lua.table(**{
             k: python_to_lua_table(lua, v)
             for k, v in obj.items()})
-    else:
+    elif isinstance(obj, list):
+        return lua.table(*[
+            python_to_lua_table(lua, v)
+            for v in obj])
+    elif isinstance(obj, int) or isinstance(obj, str) or isinstance(obj, bool) or isinstance(obj, float) or obj is None:
         return obj
+    else:
+        raise
 
 
-def image_to_data_url(image):
+def image_to_data_url(image: Image) -> str:
     stream = io.BytesIO()
     image.save(stream, 'png')
     encoded = base64.b64encode(stream.getvalue()).decode('ascii')
     return 'data:image/jpeg;base64,' + encoded
 
 
-def parse_dependencies(dependency_spec):
+def parse_dependencies(dependency_spec: str) -> tuple[str, str, str]:
     # ! for incompatibility
     # ? for an optional dependency
     # (?) for a hidden optional dependency
@@ -40,4 +54,7 @@ def parse_dependencies(dependency_spec):
     # or no prefix for a hard requirement for the other mod.
     match = re.match(r'^(?P<prefix>!|\?|\(\?\)|~)? \s* (?P<mod>.+?) \s* (?P<rest>[=>].*)?$',
                      dependency_spec, re.VERBOSE)
-    return match['prefix'], match['mod'], match['rest']
+    if match:
+        return match['prefix'], match['mod'], match['rest']
+    else:
+        raise
