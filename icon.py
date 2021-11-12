210445171727387891c0f884bd9093384273c5c8
Joey Marianer <joey@rescale.com>
2021-11-11 16:19:30 -0800

    Remove Anys from icon spec layers

--- icon.py
+++ icon.py
@@ -5,54 +5,62 @@ from PIL import Image, ImageOps
 from mod_reader import ModReader
 
 
-# TODO Get layers to not be an Any
+class RGBA(NamedTuple):
+    r: int
+    g: int
+    b: int
+    a: float = 1
+
+
+class Layer(NamedTuple):
+    icon_path: str
+    icon_size: Optional[int] = None
+    scale: float = 1
+    tint: Optional[RGBA] = None
+    shift: tuple[float, float] = (0, 0)
+
+
 class IconSpec(NamedTuple):
     size: int
-    layers: list[Any]
+    layers: list[Layer]
 
 
 def get_factorio_icon(reader: ModReader, icon_spec: IconSpec) -> Image.Image:
     icon_size = icon_spec.size
     icon = Image.new(mode='RGBA', size=(icon_size, icon_size))
-    x1: Optional[int] = None
-    x2: Optional[int] = None
-    y1: Optional[int] = None
-    y2: Optional[int] = None
+    x1: Optional[float] = None
+    x2: Optional[float] = None
+    y1: Optional[float] = None
+    y2: Optional[float] = None
     for icon_layer in icon_spec.layers:
         layer = Image \
-            .open(io.BytesIO(reader.get_binary(icon_layer['icon'])))
+            .open(io.BytesIO(reader.get_binary(icon_layer.icon_path)))
 
-        layer_original_size = icon_layer.get('icon_size', icon_size)
-        layer_scaled_size = int(layer_original_size * icon_layer.get('scale', 1))
+        layer_original_size = icon_layer.icon_size or icon_size
+        layer_scaled_size = int(layer_original_size * icon_layer.scale)
 
         layer = layer \
             .crop((0, 0, layer.height, layer.height)) \
             .convert('RGBA') \
             .resize((layer_scaled_size, layer_scaled_size))
 
-        if 'tint' in icon_layer:
-            tint = icon_layer['tint']
-            if isinstance(tint, list):
-                tint = {
-                    'r': tint[0],
-                    'g': tint[1],
-                    'b': tint[2],
-                    'a': tint[3],
-                }
+        if icon_layer.tint is not None:
+            tint = icon_layer.tint
             layer_grayscale = ImageOps.grayscale(layer).getdata()
             layer_alpha = layer.getdata(3)
 
             layer_new_data = map(
                     lambda grayscale, alpha: (
-                        int(grayscale * tint['r']),
-                        int(grayscale * tint['g']),
-                        int(grayscale * tint['b']),
-                        int(alpha * tint.get('a', 1))),
+                        int(grayscale * tint.r),
+                        int(grayscale * tint.g),
+                        int(grayscale * tint.b),
+                        int(alpha * tint.a)),
                     layer_grayscale, layer_alpha)
+
             # TODO Not sure but I think the type of layer.putdata is just wrong in the stub.
             layer.putdata(list(layer_new_data))  # type: ignore
 
-        shift_x, shift_y = icon_layer.get('shift', (0, 0))
+        shift_x, shift_y = icon_layer.shift
         default_offset = (icon_size - layer_scaled_size) / 2
         shift_x += default_offset
         shift_y += default_offset
@@ -70,26 +78,40 @@ def get_factorio_icon(reader: ModReader, icon_spec: IconSpec) -> Image.Image:
             y2 = shift_y + layer_scaled_size
 
     if x1 and x2 and y1 and y2:
-        return icon.crop((x1, y1, x2, y2))
+        return icon.crop((int(x1), int(y1), int(x2), int(y2)))
     else:
         return icon
 
 
+def get_layer(a_dict: Any) -> Layer:
+    tint = None
+    if 'tint' in a_dict:
+        a_tint = a_dict['tint']
+        if isinstance(a_dict['tint'], list):
+            tint = RGBA(*a_tint)
+        else:
+            tint = RGBA(a_tint['r'], a_tint['g'], a_tint['b'], a_tint.get('a', 1))
+
+    return Layer(icon_path=a_dict['icon'],
+                 icon_size=a_dict.get('icon_size', None),
+                 scale=a_dict.get('scale', 1),
+                 shift=a_dict.get('shift', (0, 0)),
+                 tint=tint)
+
+
 def get_icon_specs(a_dict: Any) -> IconSpec:
     if 'icon' in a_dict:
-        layers = [{
-            'icon': a_dict['icon'],
-            }]
+        layers = [Layer(icon_path=a_dict['icon'])]
     else:
-        layers = a_dict['icons']
+        layers = [get_layer(i) for i in a_dict['icons']]
 
     size = None
     if 'icon_size' in a_dict:
         size = a_dict['icon_size']
     else:
         for layer in layers:
-            if 'icon_size' in layer:
-                size = layer['icon_size']
+            if layer.icon_size is not None:
+                size = layer.icon_size
                 break
 
     if size is None:
